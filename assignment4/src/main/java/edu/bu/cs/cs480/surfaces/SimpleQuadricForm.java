6f6a8ad96a11577c6bc886d6b397d0172bb70dfb
Jeffrey Finkelstein <jeffrey.finkelstein@gmail.com>
2011-04-23 13:17:25 -0400

    added documentation and sorted members

--- assignment4/src/main/java/edu/bu/cs/cs480/surfaces/SimpleQuadricForm.java
+++ assignment4/src/main/java/edu/bu/cs/cs480/surfaces/SimpleQuadricForm.java
@@ -1,9 +1,8 @@
 /**
- * 
+ * SimpleQuadricForm.java - a surface object described by a quadric matrix
  */
 package edu.bu.cs.cs480.surfaces;
 
-import sun.awt.windows.ThemeReader;
 import edu.bu.cs.cs480.Intercept;
 import edu.bu.cs.cs480.Matrix4x4;
 import edu.bu.cs.cs480.Pair;
@@ -12,55 +11,31 @@ import edu.bu.cs.cs480.Ray;
 import edu.bu.cs.cs480.Vector4D;
 
 /**
- * @author jeff
+ * A surface object which can be described by a quadric matrix.
  * 
+ * @author Jeffrey Finkelstein <jeffrey.finkelstein@gmail.com>
+ * @since Spring 2011
  */
 public abstract class SimpleQuadricForm extends ConcreteSurfaceObject {
-
+  /**
+   * The matrix assigned by the {@link #compile()} method, which represents the
+   * quadric form of this surface object.
+   */
   private Matrix4x4 matrix = null;
 
-  protected Matrix4x4 matrix() {
-    return this.matrix;
-  }
-
   /**
-   * {
+   * The matrix of the quadric form of this surface object centered at the
+   * origin and with no rotation.
    * 
-   * @see
-   * edu.bu.cs.cs480.surfaces.SurfaceObject#interceptWith(edu.bu.cs.cs480.Ray)
+   * @return The matrix of the quadric form of this surface object centered at
+   *         the origin and with no rotation.
    */
-  @Override
-  public Intercept interceptWith(final Ray ray) {
-    final Vector4D u = new Vector4D(ray.direction(), 0);
-    final Vector4D p = new Vector4D(ray.position(), 1);
-
-    final double a = u.dotProduct(this.matrix.product(u));
-    final Vector4D temp = this.matrix.product(p);
-    final double b = 2 * u.dotProduct(temp);
-    final double c = p.dotProduct(temp);
-
-    final Pair pair = QuadraticSolver.solve(a, b, c);
-    if (pair == null) {
-      return null;
-    }
-    final double time = Math.min(pair.left(), pair.right());
-
-    return new Intercept(ray, time, this);
-  }
-
-  /**
-   * The matrix representing the rotation which is applied to this surface
-   * object.
-   * 
-   * @return The matrix representing the rotation which is applied to this
-   *         surface object.
-   */
-  protected abstract Matrix4x4 rotation();
+  protected abstract Matrix4x4 baseMatrix();
 
   /**
    * Compiles the matrix which represents the quadric form of this surface
-   * object with respect to its base matrix representation, its rotation matrix,
-   * and its translation matrix.
+   * object with respect to its base matrix representation, its rotation
+   * matrix, and its translation matrix.
    * 
    * This method MUST be called before calling the {@link #interceptWith(Ray)}
    * method.
@@ -73,10 +48,10 @@ public abstract class SimpleQuadricForm extends ConcreteSurfaceObject {
    * 
    * Algorithm: To account for the translation and rotation of this object, we
    * save time by compiling it into {@link #matrix} field. Let L be the
-   * translation matrix from the origin to the position of this object. Let R be
-   * the rotation matrix from the standard Euclidean orthonormal basis to the
-   * orthonormal basis of this object, and let Q be the quadric form matrix of
-   * this object. This method compiles and stores Q' =
+   * translation matrix from the origin to the position of this object. Let R
+   * be the rotation matrix from the standard Euclidean orthonormal basis to
+   * the orthonormal basis of this object, and let Q be the quadric form matrix
+   * of this object. This method compiles and stores Q' =
    * ((L^-1)^T)((R^-1)^T)Q(R^-1)(L^-1) in the {@link #matrix} field.
    */
   @Override
@@ -102,6 +77,33 @@ public abstract class SimpleQuadricForm extends ConcreteSurfaceObject {
   }
 
   /**
+   * {@inheritDoc}
+   * 
+   * @param ray
+   *          {@inheritDoc}
+   * @return {@inheritDoc}
+   * @see edu.bu.cs.cs480.surfaces.SurfaceObject#interceptWith(edu.bu.cs.cs480.Ray)
+   */
+  @Override
+  public Intercept interceptWith(final Ray ray) {
+    final Vector4D u = new Vector4D(ray.direction(), 0);
+    final Vector4D p = new Vector4D(ray.position(), 1);
+
+    final double a = u.dotProduct(this.matrix.product(u));
+    final Vector4D temp = this.matrix.product(p);
+    final double b = 2 * u.dotProduct(temp);
+    final double c = p.dotProduct(temp);
+
+    final Pair pair = QuadraticSolver.solve(a, b, c);
+    if (pair == null) {
+      return null;
+    }
+    final double time = Math.min(pair.left(), pair.right());
+
+    return new Intercept(ray, time, this);
+  }
+
+  /**
    * Returns a new matrix representing the inverse of the translation to the
    * center of this object as specified by the {@link #position()} method.
    * 
@@ -117,11 +119,24 @@ public abstract class SimpleQuadricForm extends ConcreteSurfaceObject {
   }
 
   /**
-   * The matrix of the quadric form of this surface object centered at the
-   * origin and with no rotation.
+   * Gets the matrix which represents the quadric form of this surface object.
    * 
-   * @return The matrix of the quadric form of this surface object centered at
-   *         the origin and with no rotation.
+   * This method returns {@code null} unless the {@link #compile()} method has
+   * been called.
+   * 
+   * @return The matrix which represents the quadric form of this surface
+   *         object.
    */
-  protected abstract Matrix4x4 baseMatrix();
+  protected Matrix4x4 matrix() {
+    return this.matrix;
+  }
+
+  /**
+   * The matrix representing the rotation which is applied to this surface
+   * object.
+   * 
+   * @return The matrix representing the rotation which is applied to this
+   *         surface object.
+   */
+  protected abstract Matrix4x4 rotation();
 }

16d44bab3cccfdd0e596b8329bd4b2d973c32226
Jeffrey Finkelstein <jeffrey.finkelstein@gmail.com>
2011-04-27 00:03:25 -0400

    added spotlight

---
+++ assignment4/src/main/java/edu/bu/cs/cs480/SpotLight.java
@@ -0,0 +1,12 @@
+/**
+ * SpotLight.java - 
+ */
+package edu.bu.cs.cs480;
+
+/**
+ * @author Jeffrey Finkelstein <jeffrey.finkelstein@gmail.com>
+ * @since Spring 2011
+ */
+public class SpotLight extends Light {
+
+}

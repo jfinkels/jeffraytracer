9eccf21f97805efe1efadbd303f296bc454222c0
Jeffrey Finkelstein <jeffrey.finkelstein@gmail.com>
2011-09-07 01:30:40 -0400

    distributed under the GPLv3

--- src/main/java/jeffraytracer/rendering/renderers/DefaultRenderer.java
+++ src/main/java/jeffraytracer/rendering/renderers/DefaultRenderer.java
@@ -1,5 +1,22 @@
 /**
  * BaseRenderer.java - renders a scene by calling a tracer
+ * 
+ * Copyright 2011 Jeffrey Finkelstein
+ * 
+ * This file is part of jeffraytracer.
+ * 
+ * jeffraytracer is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ * 
+ * jeffraytracer is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * jeffraytracer. If not, see <http://www.gnu.org/licenses/>.
  */
 package jeffraytracer.rendering.renderers;
 
